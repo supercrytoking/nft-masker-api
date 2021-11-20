14b90b1831db59f7ed26b9574383ec54c11ef845
supercrytoking <108713886+supercrytoking@users.noreply.github.com>
2021-11-20 14:46:55 +0100

    Fixed

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-    async rewrites() {
-        return [
-            {
-                source: '/api/:slug*',
-                destination: 'https://mask.fantom.digital/api/:slug*'
-            }
-        ]
-    },
     typescript: {
         ignoreBuildErrors: true
     },
