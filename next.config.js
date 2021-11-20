b9d658946e0cc06d1ac31fa52b249de9e6e754f8
supercrytoking <108713886+supercrytoking@users.noreply.github.com>
2021-11-20 14:30:43 +0100

    Changes CORS Policy Test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'https://api.example.com/:path*'
+            }
+        ]
+    },
     typescript: {
         ignoreBuildErrors: true
     },
